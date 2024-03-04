776e5b58a84483198da74ddc0392cec9c4de4f30
Drumstickz64 <gearman533@gmail.com>
2024-03-04 21:44:48 +0300

    REFACTOR: make particle properties public and remove getters and setters

--- src/particle.rs
+++ src/particle.rs
@@ -2,56 +2,14 @@ use crate::{math::vector::Vec3, precision::Real};
 
 #[derive(Debug, Clone)]
 pub struct Particle {
-    position: Vec3,
-    velocity: Vec3,
-    acceleration: Vec3,
-    damping: Real,
-    inverse_mass: Real,
+    pub position: Vec3,
+    pub velocity: Vec3,
+    pub acceleration: Vec3,
+    pub damping: Real,
+    pub inverse_mass: Real,
 }
 
 impl Particle {
-    pub fn new(mass: Real) -> Self {
-        assert_ne!(mass, 0.0, "Particles can't have zero mass");
-        let inverse_mass = if mass == Real::INFINITY {
-            0.0
-        } else {
-            mass.recip()
-        };
-
-        Self {
-            position: Vec3::ZERO,
-            velocity: Vec3::ZERO,
-            acceleration: Vec3::ZERO,
-            damping: 0.99,
-            inverse_mass,
-        }
-    }
-
-    pub fn with_position(mut self, position: Vec3) -> Self {
-        self.position = position;
-        self
-    }
-
-    pub fn with_velocity(mut self, velocity: Vec3) -> Self {
-        self.velocity = velocity;
-        self
-    }
-
-    pub fn with_acceleration(mut self, acceleration: Vec3) -> Self {
-        self.acceleration = acceleration;
-        self
-    }
-
-    pub fn with_damping(mut self, damping: Real) -> Self {
-        self.damping = damping;
-        self
-    }
-
-    pub fn with_mass(mut self, mass: Real) -> Self {
-        self.set_mass(mass);
-        self
-    }
-
     pub fn integrate(&mut self, duration: Real) {
         if self.inverse_mass <= 0.0 {
             return;
@@ -66,55 +24,7 @@ impl Particle {
         self.velocity = self.velocity * self.damping.powf(duration) + resulting_acc * duration;
     }
 
-    pub fn kinetic_energy(&self) -> Real {
-        if self.inverse_mass == 0.0 {
-            return Real::MAX;
-        }
-
-        0.5 * self.inverse_mass.recip() * self.velocity.squared_magnitude()
-    }
-
-    pub fn position(&self) -> Vec3 {
-        self.position
-    }
-
-    pub fn set_position(&mut self, position: Vec3) {
-        self.position = position;
-    }
-
-    pub fn velocity(&self) -> Vec3 {
-        self.velocity
-    }
-
-    pub fn set_velocity(&mut self, velocity: Vec3) {
-        self.velocity = velocity;
-    }
-
-    pub fn acceleration(&self) -> Vec3 {
-        self.acceleration
-    }
-
-    pub fn set_acceleration(&mut self, acceleration: Vec3) {
-        self.acceleration = acceleration;
-    }
-
-    pub fn damping(&self) -> f32 {
-        self.damping
-    }
-
-    pub fn set_damping(&mut self, damping: Real) {
-        self.damping = damping;
-    }
-
-    pub fn inverse_mass(&self) -> f32 {
-        self.inverse_mass
-    }
-
-    pub fn set_inverse_mass(&mut self, inverse_mass: Real) {
-        self.inverse_mass = inverse_mass;
-    }
-
-    pub fn get_mass(&self) -> Real {
+    pub fn mass(&self) -> Real {
         if self.inverse_mass == 0.0 {
             return Real::MAX;
         }
