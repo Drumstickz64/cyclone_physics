7d9e6e180543f8fbc4c95221859146dbca2b48c0
Drumstickz64 <gearman533@gmail.com>
2024-03-16 15:52:04 +0300

    FEAT: add springs example

---
+++ examples/springs.rs
@@ -0,0 +1,94 @@
+use cyclone_physics::{
+    math::vector::Vec3 as CVec3,
+    particle::{Particle, ParticleSet},
+    pfgen::{ForceGeneratorSet, ParticleSpring},
+    pipeline::ParticlePipeline,
+};
+
+use macroquad::prelude::*;
+
+const PLAYER_MASS: f32 = 15.0;
+const MOUSE_MASS: f32 = 5.0;
+const GRAVITY: CVec3 = CVec3::new(0.0, 250.0, 0.0);
+const PLAYER_DAMPING: f32 = 0.8;
+const SPRING_CONSTANT: f32 = 25.0;
+const SPRING_REST_LENGTH: f32 = 1.0;
+const SPRING_THICKNESS: f32 = 3.0;
+const MOUSE_PARTICLE_RADIUS: f32 = 25.0;
+const PLAYER_PARTICLE_RADIUS: f32 = 35.0;
+const MOUSE_PARTICLE_COLOR: Color = RED;
+const PLAYER_PARTICLE_COLOR: Color = BLUE;
+const SPRING_COLOR: Color = GREEN;
+
+#[macroquad::main("Springs")]
+async fn main() {
+    let mut particles = ParticleSet::with_key();
+    let mut fgs = ForceGeneratorSet::with_key();
+
+    let player_particle = particles.insert(
+        Particle::new(PLAYER_MASS)
+            .with_position(initial_player_pos())
+            .with_acceleration(GRAVITY)
+            .with_damping(PLAYER_DAMPING),
+    );
+
+    let mouse_particle = particles.insert(Particle::new(MOUSE_MASS).with_position(mouse_pos_vec()));
+
+    let spring = fgs.insert(Box::new(ParticleSpring {
+        other: mouse_particle,
+        spring_constant: SPRING_CONSTANT,
+        rest_length: SPRING_REST_LENGTH,
+    }));
+
+    let mut pipeline = ParticlePipeline::new();
+
+    pipeline.register_force(player_particle, spring);
+
+    loop {
+        clear_background(BLACK);
+
+        let duration = get_frame_time();
+
+        pipeline.step(&mut particles, &mut fgs, duration);
+
+        let mouse_pos = mouse_pos_vec();
+        let player_pos = particles[player_particle].position();
+
+        particles[mouse_particle].set_position(mouse_pos);
+
+        draw_line(
+            player_pos.x,
+            player_pos.y,
+            mouse_pos.x,
+            mouse_pos.y,
+            SPRING_THICKNESS,
+            SPRING_COLOR,
+        );
+
+        draw_circle(
+            mouse_pos.x,
+            mouse_pos.y,
+            MOUSE_PARTICLE_RADIUS,
+            MOUSE_PARTICLE_COLOR,
+        );
+
+        draw_circle(
+            player_pos.x,
+            player_pos.y,
+            PLAYER_PARTICLE_RADIUS,
+            PLAYER_PARTICLE_COLOR,
+        );
+
+        next_frame().await;
+    }
+}
+
+fn initial_player_pos() -> CVec3 {
+    CVec3::new(screen_width() / 2.0, screen_height() / 1.5, 0.0)
+}
+
+fn mouse_pos_vec() -> CVec3 {
+    let (mouse_x, mouse_y) = mouse_position();
+
+    CVec3::new(mouse_x, mouse_y, 0.0)
+}
