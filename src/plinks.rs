d678b8762097258dd0b707852a48f4f7d2d86d22
Drumstickz64 <gearman533@gmail.com>
2024-03-23 01:47:07 +0300

    FEAT: add contraint version of cables and rods

--- src/plinks.rs
+++ src/plinks.rs
@@ -72,6 +72,7 @@ impl ParticleContactGenerator for ParticleRod {
         1
     }
 }
+
 #[derive(Debug, Clone)]
 pub struct ParticleGroundCollider {
     pub particle: ParticleHandle,
@@ -93,6 +94,71 @@ impl ParticleContactGenerator for ParticleGroundCollider {
             particle_a: self.particle,
             particle_b: None,
             data: ParticleContactData::new(self.restitution, normal, penetration),
+        };
+
+        1
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ParticleCableConstraint {
+    pub particle_a: ParticleHandle,
+    pub anchor: Vec3,
+    pub max_length: Real,
+    pub restitution: Real,
+}
+
+impl ParticleContactGenerator for ParticleCableConstraint {
+    fn add_contacts(&self, contacts: &mut [ParticleContact], particles: &ParticleSet) -> usize {
+        let position_a = particles[self.particle_a].position;
+
+        let length = position_a.distance_to(self.anchor);
+        if length < self.max_length {
+            return 0;
+        }
+
+        let normal = position_a.direction_to(self.anchor);
+        let penetration = length - self.max_length;
+
+        contacts[0] = ParticleContact {
+            particle_a: self.particle_a,
+            particle_b: None,
+            data: ParticleContactData::new(self.restitution, normal, penetration),
+        };
+
+        1
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ParticleRodConstraint {
+    pub particle_a: ParticleHandle,
+    pub anchor: Vec3,
+    pub length: Real,
+}
+
+impl ParticleContactGenerator for ParticleRodConstraint {
+    fn add_contacts(&self, contacts: &mut [ParticleContact], particles: &ParticleSet) -> usize {
+        let position_a = particles[self.particle_a].position;
+
+        let curr_length = position_a.distance_to(self.anchor);
+        if curr_length == self.length {
+            return 0;
+        }
+
+        let normal = if curr_length > self.length {
+            position_a.direction_to(self.anchor)
+        } else {
+            self.anchor.direction_to(position_a)
+        };
+
+        let penetration = (curr_length - self.length).abs();
+        let restitution = 0.0;
+
+        contacts[0] = ParticleContact {
+            particle_a: self.particle_a,
+            particle_b: None,
+            data: ParticleContactData::new(restitution, normal, penetration),
         };
 
         1
